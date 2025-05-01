d2bb1b2154c54b7a7aec7c834f3fe62252278139
progressmachine <pranish.school@gmail.com>
2025-05-01 15:19:18 +0545

    Add files via upload

---
+++ input/madlibs.py
@@ -0,0 +1,10 @@
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+adjective3 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun}.")
+print(f"{noun} was {adjective2} and {verb}ing.")
+print(f"I was {adjective3}.")
