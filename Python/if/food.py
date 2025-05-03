bee74b1423b734aa796ace254f1b7e0eacbf7810
progressmachine <pranish.school@gmail.com>
2025-05-03 21:40:08 +0545

    Add files via upload

---
+++ Python/if/food.py
@@ -0,0 +1,6 @@
+response = input("Are you hungry? (Y/N): ")
+
+if response.capitalize == "Y":
+    print("Then eat.")
+else:
+    print("Then don't eat.")
---
+++ Python/if/online.py
@@ -0,0 +1,6 @@
+online = False
+
+if online:
+    print("The user is online.")
+else:
+    print("The user is offline.")
