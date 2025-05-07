aced22bd84d58cd57cd5f0e9e4e24c1b61f43551
progressmachine <pranish.school@gmail.com>
2025-05-07 21:33:30 +0545

    Add files via upload

---
+++ Python/range/main.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    print(i) # 0 to 4
+
+print()
+
+for i in range(2, 6):
+    print(i) # 2 to 5
+
+print()
+
+for i in range(1, 11, 2):
+    print(i) # 1 3 5 7 9 not 11 cause remember - exclusive
+
+print()
+
+for i in range(10, 0, -2):
+    print(i) # 10 8 6 4 2
+
+print()
+
+for i in range(10, -1, -2):  # stop at -1 so 0 is included
+    print(i) # 10 8 6 4 2 0
